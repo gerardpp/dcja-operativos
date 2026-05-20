2f7c9af104915d853564a501faebbd2c424ad15a
Cashgp <124806321+Cashgp@users.noreply.github.com>
2026-05-20 12:46:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1748,4 +1748,4 @@ def mapa_view():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False) 
+    app.run(host='0.0.0.0', port=port, debug=False)
